f2c944f45e13f1814c5477233668404f3658178e
Cindy Liao <cindy.liao@d1g1t.com>
2021-08-08 18:38:03 -0400

    Add mutaiton type and related logic

--- main/schemas.py
+++ main/schemas.py
@@ -6,7 +6,7 @@ from graphql import GraphQLResolveInfo
 from main.models import Author, Post
 
 from ariadne import QueryType, make_executable_schema, gql, ObjectType, \
-    snake_case_fallback_resolvers
+    snake_case_fallback_resolvers, MutationType
 
 type_defs = gql("""
     type Query {
@@ -15,29 +15,49 @@ type_defs = gql("""
     }
     
     type AuthorType {
-        id: Int!
+        id: ID!
         firstName: String
         lastName: String
         email: String!
-        posts: [PostType!]!
+        posts: [PostType!]
     }
     
     type PostType{
-        id: Int!
+        id: ID!
         title: String!
         contents: String!
         author: AuthorType!
     }
     
+    type Mutation {
+        createOrUpdateAuthor(input: AuthorInput!): AuthorType
+        createOrUpdatePost(input: PostInput!): PostType
+    }
+    
+    input AuthorInput {
+        id: ID
+        firstName: String
+        lastName: String
+        email: String
+    }
+    
+    input PostInput {
+        id: ID
+        title: String
+        contents: String
+        author_id: ID
+    }
 """)
 
 # Create ObjectType instance for Query type defined in our schema...
 query = QueryType()
-user_type = ObjectType("AuthorType")
+author_type = ObjectType("AuthorType")
 post_type = ObjectType("PostType")
 
+mutation = MutationType()
 
-# bind resolvers to its field
+
+# bind resolvers to its field defined on type_defs
 @query.field("allAuthors")
 def resolve_all_author(obj: Any, info: GraphQLResolveInfo):
     """
@@ -51,9 +71,34 @@ def resolve_all_author(obj: Any, info: GraphQLResolveInfo):
     return Author.objects.all()
 
 
+@author_type.field("posts")
+def resolve_posts(obj, info):
+    return obj.posts.all()
+
+
 @query.field("authorByEmail")
 def resolve_all_author(*_, email):
     return Author.objects.get(email=email)
 
 
-schema = make_executable_schema(type_defs, [query, user_type, post_type, snake_case_fallback_resolvers])
+@mutation.field("createOrUpdateAuthor")
+def resolve_create_or_update_author(*_, input):
+    id = input.pop("id", None)
+    if id:
+        author, _ = Author.objects.update_or_create(id=id, defaults=input)
+    else:
+        author, _ = Author.objects.update_or_create(**input)
+    return author
+
+
+@mutation.field("createOrUpdatePost")
+def resolve_create_or_update_post(*_, input):
+    id = input.pop("id", None)
+    if id:
+        post, _ = Post.objects.update_or_create(id=id, defaults=input)
+    else:
+        post, _ = Post.objects.update_or_create(**input)
+    return post
+
+
+schema = make_executable_schema(type_defs, [query, author_type, post_type, mutation, snake_case_fallback_resolvers])
